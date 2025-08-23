2ab78db391d4b8bcbd4685e94816c9636d4584c9
Kuchuk borom Debbarma <kuchukboromd@outlook.com>
2025-08-23 21:01:28 +0530

    Initial commit

---
+++ src/main/java/dev/kuku/vfl/vfl_hub/VflHubApplication.java
@@ -0,0 +1,13 @@
+package dev.kuku.vfl.vfl_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VflHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VflHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kuku/vfl/vfl_hub/VflHubApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kuku.vfl.vfl_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VflHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
